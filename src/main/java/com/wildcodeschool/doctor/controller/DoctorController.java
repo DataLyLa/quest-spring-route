52eaace6548ebeb2b540a554c4d6718b42018725
Lyndia Laurole <lyndia.laurole@gmail.com>
2023-06-20 17:30:42 +0200

    end of exercice

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,9 +1,13 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
@@ -14,4 +18,17 @@ public class DoctorController {
 
         return null;
     }
+
+    @GetMapping("/doctor/{number}")
+    @ResponseBody
+    Doctor doctor(@PathVariable int number){
+        if(number==13){
+            return new Doctor(13, "Jodie Whittaker");
+        }
+        if(number==42){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation {number}");
+        }
+        throw new ResponseStatusException(HttpStatus.SEE_OTHER, "See other.");
+    }
+    
 }
